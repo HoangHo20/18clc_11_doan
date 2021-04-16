9066969dc6234a93c1d27e8fad5a32d8d6e90c73
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2021-04-16 17:41:54 +0700

    + Thêm ảnh từ điện thoại vào album

--- app/src/main/java/com/example/imagealbum/showAlbum.java
+++ app/src/main/java/com/example/imagealbum/showAlbum.java
@@ -71,4 +71,20 @@ public class showAlbum extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data.getClipData() != null) {
+                    int count = data.getClipData().getItemCount();
+                    for (int i = 0; i < count; i++) {
+                        uri.add(data.getClipData().getItemAt(i).getUri());
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+                else if (data.getData() != null) {
+                    String imagePath = data.getData().getPath();
+                }
+            }
+        }
+    }
 }
