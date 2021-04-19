ce0f5da09a8a20ab1aabbe29f76ec3a8291528a4
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2021-04-19 21:24:15 +0700

    ! cấu trúc của image

---
+++ app/src/main/java/com/example/imagealbum/image.java
@@ -0,0 +1,63 @@
+package com.example.imagealbum;
+
+import android.net.Uri;
+
+import com.google.gson.Gson;
+
+public class image {
+    private String uri;
+    private long size;
+    private String name;
+    private String location;
+    private String date;
+    private String path;
+
+    public image(Uri uri, long size, String name, String location, String date, String path){
+        this.uri = uri.toString();
+        this.size = size;
+        this.name = name;
+        this.location = location;
+        this.date = date;
+        this.path = path;
+    }
+
+    public Uri getImage_URI() {
+        return Uri.parse(uri);
+    }
+
+    public String getImage_name(){return name;}
+
+    public long getImage_size() {
+        return size;
+    }
+
+    public String getLocation(){return this.location;}
+
+    public String getDate(){return this.date;}
+
+    public void setImage_uri(Uri URI) {
+        this.uri = URI.toString();
+    }
+
+    public void setImage_size(int size){
+        this.size = size;
+    }
+
+    public void setImage_name(String name){
+        this.name = name;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public String toJson(){
+        String json = new Gson().toJson(this);
+        return json;
+    }
+}
+
